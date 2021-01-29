105fc000311ba9a1264ace67c533a1b54f74611f
Denver Coneybeare <dconeybe@google.com>
2021-01-29 11:35:48 -0500

    Added buttons to query a document, a collection, and to create documents

--- app/src/main/java/com/google/dconeybe/MainActivity.java
+++ app/src/main/java/com/google/dconeybe/MainActivity.java
@@ -11,17 +11,24 @@ import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.lang.ref.WeakReference;
+import java.util.Date;
+import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final WeakReference<MainActivity> selfRef = new WeakReference<>(this);
 
     private FirebaseFirestore firestore;
-    private Button buttonView;
+    private Button documentGetButtonView;
+    private Button collectionGetButtonView;
+    private Button createDocumentsButtonView;
     private TextView textView;
     private boolean getInProgress;
 
@@ -32,8 +39,12 @@ public class MainActivity extends AppCompatActivity {
         FirebaseFirestore.setLoggingEnabled(true);
         firestore = FirebaseFirestore.getInstance();
         setContentView(R.layout.activity_main);
-        buttonView = requireViewById(R.id.button);
-        buttonView.setOnClickListener(new ButtonClickListener());
+        documentGetButtonView = requireViewById(R.id.document_get_button);
+        documentGetButtonView.setOnClickListener(this);
+        collectionGetButtonView = requireViewById(R.id.collection_get_button);
+        collectionGetButtonView.setOnClickListener(this);
+        createDocumentsButtonView = requireViewById(R.id.documents_create_button);
+        createDocumentsButtonView.setOnClickListener(this);
         textView = requireViewById(R.id.text);
         Log.e("zzyzx", "MainActivity.onCreate() done");
     }
@@ -46,47 +57,166 @@ public class MainActivity extends AppCompatActivity {
         Log.e("zzyzx", "MainActivity.onDestroy() done");
     }
 
-    void onButtonClick() {
+    @Override
+    public void onClick(View view) {
+        if (view == documentGetButtonView) {
+            onDocumentGetButtonClick();
+        } else if (view == collectionGetButtonView) {
+            onCollectionGetButtonClick();
+        } else if (view == createDocumentsButtonView) {
+            onCreateDocumentsButtonClick();
+        } else {
+            throw new IllegalArgumentException("unknown view: " + view);
+        }
+    }
+
+    void onDocumentGetButtonClick() {
         if (getInProgress) {
             return;
         }
-        firestore.document("/AndroidIssue2311/doc001").get().addOnCompleteListener(new FirestoreGetOnCompleteListener(selfRef));
+        firestore.document("/AndroidIssue2311/doc001").get().addOnCompleteListener(new DocumentGetOnCompleteListener(selfRef));
         textView.setText("Retrieving document...");
         getInProgress = true;
     }
 
-    void onFirestoreGetComplete(Task<DocumentSnapshot> task) {
+    void onDocumentGetComplete(Task<DocumentSnapshot> task) {
         getInProgress = false;
         Exception exception = task.getException();
         DocumentSnapshot result = task.getResult();
         if (result != null) {
-            textView.setText("Successfully retrieved " + result.getReference().getPath() + "; date=" + result.get("date"));
+            if (!result.exists()) {
+                textView.setText("Document " + result.getReference().getPath() + " does not exist");
+            } else {
+                textView.setText("Successfully retrieved " + result.getReference().getPath() + "; date=" + result.get("date"));
+            }
         } else {
             textView.setText("Failed to retrieve document: " + exception);
         }
     }
 
-    private final class ButtonClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            onButtonClick();
+    void onCollectionGetButtonClick() {
+        if (getInProgress) {
+            return;
+        }
+        firestore.collection("/AndroidIssue2311").whereGreaterThanOrEqualTo("index", 0).get().addOnCompleteListener(new CollectionGetOnCompleteListener(selfRef));
+        textView.setText("Retrieving collection...");
+        getInProgress = true;
+    }
+
+    void onCollectionGetComplete(Task<QuerySnapshot> task) {
+        getInProgress = false;
+        Exception exception = task.getException();
+        QuerySnapshot result = task.getResult();
+        if (result != null) {
+            textView.setText("Successfully retrieved " + result.size() + " documents");
+        } else {
+            textView.setText("Failed to retrieve collection: " + exception);
         }
     }
 
-    private static class FirestoreGetOnCompleteListener implements OnCompleteListener<DocumentSnapshot> {
+    void onCreateDocumentsButtonClick() {
+        if (getInProgress) {
+            return;
+        }
+        int numDocumentsToCreate = 50;
+        textView.setText("Creating " + numDocumentsToCreate + " documents...");
+        CollectionReference collection = firestore.collection("/AndroidIssue2311");
+        DocumentCreateOnCompleteListener completionListener = new DocumentCreateOnCompleteListener(selfRef, numDocumentsToCreate);
+        for (int i=0; i<numDocumentsToCreate; i++) {
+            String nameIndex = Integer.toString(i);
+            while (nameIndex.length() < 3) {
+                nameIndex = "0" + nameIndex;
+            }
+            DocumentReference doc = collection.document("doc" + nameIndex);
+
+            HashMap<String, Object> data = new HashMap<>();
+            data.put("index", i);
+            data.put("date", new Date().toString());
+            doc.set(data).addOnCompleteListener(completionListener);
+        }
+        getInProgress = true;
+    }
+
+    void onDocumentCreateComplete(int successCount, int failCount, int expectedCount, String errorMessage) {
+        if (errorMessage != null) {
+            Log.e("zzyzx", "Failed to create document: " + errorMessage);
+        }
+        if (successCount + failCount == expectedCount) {
+            getInProgress = false;
+            if (failCount == 0) {
+                textView.setText("Document creation complete: " + successCount + " documents created");
+            } else {
+                textView.setText("Document creation complete: " + successCount + " documents created, " + failCount + " documents FAILED to be created");
+            }
+        } else if (failCount == 0) {
+            textView.setText(successCount + " documents created");
+        } else if (successCount == 0) {
+            textView.setText(failCount + " documents FAILED to be created");
+        } else {
+            textView.setText(successCount + " documents created, " + failCount + " documents FAILED to be created");
+        }
+    }
+
+    private static class DocumentGetOnCompleteListener implements OnCompleteListener<DocumentSnapshot> {
 
         private final WeakReference<MainActivity> activityRef;
 
-        FirestoreGetOnCompleteListener(WeakReference<MainActivity> activityRef) {
+        DocumentGetOnCompleteListener(WeakReference<MainActivity> activityRef) {
             this.activityRef = activityRef;
         }
 
         @Override
         public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-            Log.e("zzyzx", "FirestoreGetOnCompleteListener.onComplete() task=" + task);
             MainActivity activity = activityRef.get();
             if (activity != null) {
-                activity.onFirestoreGetComplete(task);
+                activity.onDocumentGetComplete(task);
+            }
+        }
+    }
+
+    private static class CollectionGetOnCompleteListener implements OnCompleteListener<QuerySnapshot> {
+
+        private final WeakReference<MainActivity> activityRef;
+
+        CollectionGetOnCompleteListener(WeakReference<MainActivity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+            MainActivity activity = activityRef.get();
+            if (activity != null) {
+                activity.onCollectionGetComplete(task);
+            }
+        }
+    }
+
+    private static class DocumentCreateOnCompleteListener implements OnCompleteListener<Void> {
+
+        private final WeakReference<MainActivity> activityRef;
+        private final int numDocumentsToCreate;
+        private int successCount = 0;
+        private int failCount = 0;
+
+        DocumentCreateOnCompleteListener(WeakReference<MainActivity> activityRef, int numDocumentsToCreate) {
+            this.activityRef = activityRef;
+            this.numDocumentsToCreate = numDocumentsToCreate;
+        }
+
+        @Override
+        public void onComplete(@NonNull Task<Void> task) {
+            MainActivity activity = activityRef.get();
+            if (activity != null) {
+                String errorMessage;
+                if (task.isSuccessful()) {
+                    successCount++;
+                    errorMessage = null;
+                } else {
+                    failCount++;
+                    Exception exception = task.getException();
+                    errorMessage = (exception == null) ? null : exception.toString();
+                }
+                activity.onDocumentCreateComplete(successCount, failCount, numDocumentsToCreate, errorMessage);
             }
         }
     }
